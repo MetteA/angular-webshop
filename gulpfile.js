b83834a69e36dd7c12162082df0943d839b5eace
Mette Krogsgaard Andersen <MetteKA@Mettes-MacBook-Pro-2.local>
2015-02-25 22:23:47 +0100

    With Gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,36 @@
-var gulp = require("gulp");
+var gulp = require("gulp"),
+	minifyCSS = require('gulp-minify-css');
+	browserSync = require('browser-sync');
 
-gulp.task("Hallo", function(){
-	console.log(" HEY DU");
-})
+//Minimerer css filer
+gulp.task('minify-css', function(){
+	return gulp.src('Angular-Webshop/css/*.css')
+	.pipe(minifyCSS({keepBreaks:true}))
+	.pipe(gulp.dest('Angular-Webshop/css/min-css'))
+});
+
+//synkroniserer browseren Firefox
+gulp.task('browser-sync', function(){
+	var config = {
+		server: {
+			baseDir: './Angular-Webshop/'
+		},
+		browser: ["firefox"]
+	}
+	browserSync(config);
+});
+
+//holder øje med æmndringer i html og js filer, og køre derefter tarsken browser-sync
+gulp.task("watch", function(){
+	gulp.watch([
+			"./Angular-Webshop/**.html", 
+			'./Angular-Webshop/**/*.js',
+			'./Angular-Webshop/**/*.css'
+		], 
+		["minify-css", "browser-sync", browserSync.reload]);
+});
+
+//Default: køre alle de tasks i array'et på en gang
+gulp.task("default", ['minify-css', 'browser-sync', 'watch']);
+
+
